424c28c22aad01ecffe38249ab109b2677fd3b29
GordonsBeard <thisisgordonsemail@gmail.com>
2015-08-26 18:05:57 -0700

    First real commit

---
+++ DocBot/docbot_core/docbot.py
@@ -0,0 +1,29 @@
+﻿#!/usr/bin/python
+
+# Setup and connect the bot to the server, pass along strings to docbot_core
+
+from ircutils3 import bot
+from docbot_core import response
+
+# Settings
+BOT_NAME = "DR"
+NETWORK = "irc.gamesurge.net"
+CHANNELS = ["#thefuture",]
+
+class DocBot(bot.SimpleBot):
+    def on_welcome(self, event):
+        for chan in CHANNELS:
+            self.join(chan)
+
+    def on_join(self, event):
+        pass
+    
+    def on_message(self, event):
+        payload = response(event)
+        if payload['message'] != "":
+            self.send_message(event.target, payload['message'])
+
+if __name__ == "__main__":
+    docbot = DocBot(BOT_NAME)
+    docbot.connect(NETWORK)
+    docbot.start()
---
+++ DocBot/docbot_core/docbot_core.py
@@ -0,0 +1,27 @@
+﻿#!/usr/bin/python
+import sys
+
+# pass in a string and where it came from, return the string to send, and the target
+
+def response(event):
+    final_message = "Message received."
+    payload = {"target" : event.target, "message" : final_message}
+
+    return payload
+
+if __name__ == "__main__":
+    class Event:
+        def __init__(self, target, message):
+            self.target = target
+            self.message = message
+
+    print("Debug Mode: Enter a message to parse. q = quit")
+
+    while True:
+        message = input('>>> ')
+        event = Event(target = "self", message = message)
+        if message == "q": break
+        payload = response(event)
+        print(payload['message'])
+
+    sys.exit()
